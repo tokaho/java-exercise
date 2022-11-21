d16058841148f064f4269be81512a9f432e8b6b3
tokaho <tokaho2000@gmail.com>
2022-11-21 17:00:57 +0800

    ..

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 import model.Student;
-public class Test {
+public class Main {
     public static void main(String[] args) {
         Student s1 = new Student();
         System.out.println(s1.getAge());
